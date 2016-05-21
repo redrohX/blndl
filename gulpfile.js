9d16559114864b5538bbdcc8dc8af0735fef1b48
Niels van Rongen <vanrongen@gmail.com>
2016-05-21 10:28:06 +0200

    browsersync added

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,41 @@
 
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function() {
-  return gulp.src('src/styles.css')
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('dist'));
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./scss/**/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 gulp.task('sass', function () {
- return gulp.src('./scss/**/*.scss')
+ return gulp.src('scss/**/*.scss')
   .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
+  .pipe(sass())
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./css'));
+  .pipe(gulp.dest('css'))
+  .pipe(browserSync.stream());
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./scss/**/*.scss', ['sass']);
 });
+
+gulp.task('default', ['serve']);
+
+// gulp.task('default', function() {
+//   return gulp.src('css/styles.css')
+// 		.pipe(autoprefixer({
+// 			browsers: ['last 2 versions'],
+// 			cascade: false
+// 		}))
+// 		.pipe(gulp.dest('dist'));
+// });
