270e5f2ac4e253731fd9949262d1d05195b38af8
Niels van Rongen <vanrongen@gmail.com>
2016-05-21 14:24:12 +0200

    Autoprefixer added combined with Sass, sourcemaps and browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-'use strict'
-
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+
+function onError(err) {
+  console.log(err);
+}
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -18,11 +20,12 @@ gulp.task('serve', ['sass'], function() {
 });
 
 gulp.task('sass', function () {
- return gulp.src('scss/**/*.scss')
+ return gulp.src('scss/styles.scss')
   .pipe(sourcemaps.init())
   .pipe(sass())
+  .pipe(autoprefixer('last 2 versions'))
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest('css'))
+  .pipe(gulp.dest('css/'))
   .pipe(browserSync.stream());
 });
 
@@ -31,12 +34,3 @@ gulp.task('sass:watch', function () {
 });
 
 gulp.task('default', ['serve']);
-
-// gulp.task('default', function() {
-//   return gulp.src('css/styles.css')
-// 		.pipe(autoprefixer({
-// 			browsers: ['last 2 versions'],
-// 			cascade: false
-// 		}))
-// 		.pipe(gulp.dest('dist'));
-// });
