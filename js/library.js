9f43ede53bf9a189f88499ce5df1be41faa050ec
Niels van Rongen <vanrongen@gmail.com>
2016-05-22 18:20:20 +0200

    Changed JS indentation and small changes to library

--- js/library.js
+++ js/library.js
@@ -1,35 +1,38 @@
-'use strict';
+//
+// $Blendle Library
+//
 
-var BLENDLE = (function() {
-  return {
-    toggleClass: function (element, className) {
-      var classString = element.className,
-          nameIndex = classString.indexOf(className);
+var $B = (function() {
+    'use strict';
 
-      if (!element || !className){
-        return;
-      }
+    return {
+        toggleClass: function (element, className) {
+            var classString = element.className,
+            nameIndex = classString.indexOf(className);
 
-      if (nameIndex == -1) {
-        classString += ' ' + className;
-        classString = classString.replace(/ +/g, ' ');
-      } else {
-        classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length).replace(/ +/g, ' ');
-      }
+            if (!element || !className){
+                return;
+            }
 
-      element.className = classString;
-    },
+            if (nameIndex == -1) {
+                classString += ' ' + className;
+                classString = classString.replace(/ +/g, ' ');
+            } else {
+                classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length).replace(/ +/g, ' ');
+            }
 
-    hasClass: function (element, cls) {
-      return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-    },
+            element.className = classString;
+        },
 
-    removeClass: function (element, cls) {
-      if (this.hasClass(element, cls)) {
-          var reg = new RegExp('(\\s|^)'+ cls +'(\\s|$)');
-          console.log(element);
-          element.className = element.className.replace(reg,' ');
-      }
+        hasClass: function (element, cls) {
+            return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+        },
+
+        removeClass: function (element, cls) {
+            if (this.hasClass(element, cls)) {
+                var reg = new RegExp('(\\s|^)'+ cls +'(\\s|$)');
+                element.className = element.className.replace(reg,' ');
+            }
+        }
     }
-  }
 })();
--- js/open-article.js
+++ js/open-article.js
@@ -1,31 +1,37 @@
-'use strict';
-
 var ARTICLES = (function(){
-  var articleSnippet = document.getElementsByClassName('js-article-snippet'),
-      articleReader = document.getElementsByClassName('js-article-reader'),
-      articleReaderClassName = articleReader.className,
-      visibleClassName = 'is-visible';
+    'use strict';
 
-  return {
-    openArticle: function () {
-      for (var i = 0, x = articleSnippet.length; i < x; i++) {
-        articleSnippet[i].onclick = function() {
-          BLENDLE.toggleClass(articleReader[0], visibleClassName);
-        }
-      }
-    },
+    var articleSnippet = document.getElementsByClassName('js-article-snippet'),
+    articleReader = document.getElementsByClassName('js-article-reader'),
+    articleReaderClassName = articleReader.className,
+    visibleClassName = 'is-visible',
+    moreButton = document.getElementsByClassName('js-article-snippet-more');
 
-    closeArticle: function () {
-        articleReader[0].onclick = function() {
-          if (BLENDLE.hasClass(articleReader[0], visibleClassName)) {
-            BLENDLE.removeClass(articleReader[0], visibleClassName);
-          } else {
-            console.log('2: ' + 'nope');
-          }
+    return {
+        openArticle: function () {
+            for (var i = 0, x = articleSnippet.length; i < x; i++) {
+                articleSnippet[i].onclick = function() {
+                    $B.toggleClass(articleReader[0], visibleClassName);
+                }
+            }
+        },
+
+        closeArticle: function () {
+            articleReader[0].onclick = function() {
+                if ($B.hasClass(articleReader[0], visibleClassName)) {
+                    $B.removeClass(articleReader[0], visibleClassName);
+                }
+            }
+        },
+
+        openMore: function () {
+            moreButton[0].onclick = function(event) {
+                event.stopPropagation();
+            }
         }
-    }
-  };
+    };
 })();
 
 ARTICLES.openArticle();
 ARTICLES.closeArticle();
+ARTICLES.openMore();
