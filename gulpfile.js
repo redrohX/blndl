b09b712dd76ccd7af1a7352ce66453e7f118631f
Niels van Rongen <vanrongen@gmail.com>
2016-05-26 20:35:59 +0200

    Small file and structure changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var imagemin = require('gulp-imagemin');
+var useref = require('gulp-useref');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('dev', ['sass'], function() {
 
     browserSync.init({
         server: "./"
@@ -14,26 +15,23 @@ gulp.task('serve', ['sass'], function() {
 
     gulp.watch("./scss/**/*.scss", ['sass']);
     gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("./js/**/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('sass', function () {
- return gulp.src('scss/styles.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass())
-  .pipe(autoprefixer('last 3 versions'))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('css/'))
-  .pipe(browserSync.stream());
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
+    return gulp.src('scss/styles.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoprefixer('last 3 versions'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css/'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('images', () =>
-	gulp.src('img/src/**/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('img/'))
+    gulp.src('img/src/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('img/'))
 );
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['dev']);
